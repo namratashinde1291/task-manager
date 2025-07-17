4719f1adc9666bcb1334bbc77858467752ca5615
namrata <namrata.shinde@loantap.in>
2025-07-17 16:49:16 +0530

    auth middleware | added

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -4,4 +4,16 @@ const authenticate = (req,res,next) => {
     const authHeader  = req.headers.autherization;
     if(!authHeader || !authHeader.startsWith('Bearer '))
         return res.status(400).json({message:'No token provided'});
-}
+    const token = authHeader.split(' ')[1];
+
+    try{
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    }
+    catch(err){
+        return res.status(401).json({message:'Invalid or expired token'});
+    }
+};
+
+module.exports =authenticate;
